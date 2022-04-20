5bdded4b6700505782dad9f76fb9c6425819df5b
Guilherme Fialho <guilhermec.fialho@gmail.com>
2022-04-20 11:52:21 -0300

    Correcting Absolute Imports

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ const path = require('path');
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, '/src') }],
-  },
+    alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
+  }
 })
