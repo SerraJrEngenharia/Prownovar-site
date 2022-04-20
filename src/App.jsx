2341fa4ef9fc00e3594f8efa8a424cb8c9600ec9
Guilherme Fialho <guilhermec.fialho@gmail.com>
2022-04-20 11:54:52 -0300

    Correcting Absolute Imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
-import Home from './pages/home'
-import AboutUs from './pages/aboutUs'
-import Contacts from './pages/contacts'
-import Services from './pages/services'
+import Home from '@/pages/home'
+import AboutUs from '@/pages/aboutUs'
+import Contacts from '@/pages/contacts'
+import Services from '@/pages/services'
 
 function App() {
   return (
