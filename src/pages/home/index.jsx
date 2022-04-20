46b9f0c73d1df53a7d2933da2c65cdaba3bff821
Guilherme Fialho <guilhermec.fialho@gmail.com>
2022-04-20 13:08:00 -0300

    Testing header fixed

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,13 +4,13 @@ import Header from '@/components/Header'
 import MeetCompany from '@/components/MeetCompany';
 
 // Information
-import { homeInfo } from "@/services/homeInfo"
+import { homeInfo } from "@/services/homeInfo";
 
 export default function Home(){
     return(
-        <>
+        <body>
             <Header path="home"/>
             <MeetCompany title={homeInfo.meetCompany.title} content={homeInfo.meetCompany.content} />
-        </>
+        </body>
     )
 }
