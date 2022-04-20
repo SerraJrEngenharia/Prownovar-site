ed3d7d42e01175560325b46cdff25e4318953a5a
Guilherme Fialho <guilhermec.fialho@gmail.com>
2022-04-20 11:51:23 -0300

    Adding structure to home informations

---
+++ src/services/homeInfo.js
@@ -0,0 +1,6 @@
+export const homeInfo = {
+    meetCompany : {
+        title: "Conheça a PROWNOVAR",
+        content: "Nós somos a empresa Pagil Serrana auto peças LTDA, trabalhamos no ramo de aluguel de equipamentos para serviços de terraplanagem e também atuamos na área de venda de peças para equipamentos pesados (Retroescavadeiras) e na fabricação de mangueiras hidráulicas, o que nos destaca dentro deste ramo, estamos no mercado desde 1997. O nome PAGIL, vem da união dos nomes dos sócios Paulo e Gilson, os quais foram os principais responsáveis para o surgimento da empresa. Foi através do esforço de 4 sócios onde começaram através de caminhões para aluguel e se tornando hoje essa grande loja e prestadora de serviço a 24 anos."
+    }
+}
