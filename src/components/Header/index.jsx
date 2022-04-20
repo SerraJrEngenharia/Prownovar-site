bc5ed73b34d25c875b672d868b6f98933da9bee2
Guilherme Fialho <guilhermec.fialho@gmail.com>
2022-04-20 10:08:33 -0300

    Adding style and Strong Redirect Link

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,26 @@
 import { Link } from 'react-router-dom'
 
-export default function Header(){
+import "./style.css";
+
+
+import { PrownovarLogo } from '../PrownovarLogo';
+
+
+
+export default function Header(props){
     return(
-        <div>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/Sobre">Sobre nós</Link></li>
-                <li><Link to="/Servicos">Serviços</Link></li>
-                <li><Link to="/Contatos">Contatos</Link></li>
-            </ul>
+        <div className="header-container">
+            <div className="header-container-logo">
+                <PrownovarLogo />
+            </div>
+            <nav className="header-container-nav">
+                <ul className="header-container-nav-ul">
+                    <li className="header-container-nav-ul-li">{ props.path == "home" ? <strong><Link to="/">Home</Link></strong> : <Link to="/">Home</Link>}</li>
+                    <li className="header-container-nav-ul-li">{ props.path == "aboutUs" ? <strong><Link to="/Sobre">Sobre nós</Link></strong> : <Link to="/Sobre">Sobre nós</Link>}</li>
+                    <li className="header-container-nav-ul-li">{ props.path == "services" ? <strong><Link to="/Servicos">Serviços</Link></strong> : <Link to="/Servicos">Serviços</Link>}</li>
+                    <li className="header-container-nav-ul-li">{ props.path == "contact" ? <strong><Link to="/Contatos">Contatos</Link></strong> : <Link to="/Contatos">Contatos</Link>}</li>
+                </ul>
+            </nav>
         </div>
     )
 }
